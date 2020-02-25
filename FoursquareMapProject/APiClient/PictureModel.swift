1a89dff4ce4a65001282faa6da5c85b7852571d2
tanyaburke <tanyaburke@pursuit.org>
2020-02-25 10:30:19 -0500

    Created Models,Still working on API

---
+++ FoursquareMapProject/APiClient/PictureModel.swift
@@ -0,0 +1,9 @@
+//
+//  PictureModel.swift
+//  FoursquareMapProject
+//
+//  Created by Tanya Burke on 2/24/20.
+//  Copyright © 2020 Margiett Gil. All rights reserved.
+//
+
+import Foundation
---
+++ FoursquareMapProject/APiClient/SearchModel.swift
@@ -0,0 +1,9 @@
+//
+//  SearchModel.swift
+//  FoursquareMapProject
+//
+//  Created by Tanya Burke on 2/24/20.
+//  Copyright © 2020 Margiett Gil. All rights reserved.
+//
+
+import Foundation
