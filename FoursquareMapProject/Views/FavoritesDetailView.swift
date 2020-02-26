74d3f732d53b87c49e7ae233f348e5a5741ba7a7
Margiett Gil <mxg627@icloud.com>
2020-02-26 15:21:34 -0500

    figuring detail

---
+++ FoursquareMapProject/Views/FavoritesDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesDetailView.swift
+//  FoursquareMapProject
+//
+//  Created by Margiett Gil on 2/26/20.
+//  Copyright © 2020 Margiett Gil. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
