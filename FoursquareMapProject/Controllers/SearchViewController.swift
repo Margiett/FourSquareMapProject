288b19f5ee77e28a05be16069e1c14654efd7032
David <lin.david985@gmail.com>
2020-02-28 15:37:38 -0500

    pushing map changes

--- FoursquareMapProject/Controllers/SearchViewController.swift
+++ FoursquareMapProject/Controllers/SearchViewController.swift
@@ -8,65 +8,173 @@
 
 import UIKit
 import DataPersistence
+import MapKit
+import CoreLocation
+
 class SearchViewController: UIViewController {
-   
-   
-  let searchView = SearchView()
-   
-  private var dataPersistence: DataPersistence<Venue>?
-   
-  private var userPreference: UserPreference?
-   
-  private var allLocations = [String]() {
-    didSet{
-      DispatchQueue.main.async {
-        self.searchView.collectionView.reloadData()
-      }
+    
+    
+    let searchView = SearchView()
+    
+    private var dataPersistence: DataPersistence<Venue>?
+    
+    private var userPreference: UserPreference?
+    
+    private let coreLocation = CoreLocationSession()
+    
+    private var annotation = [MKAnnotation]()
+    
+    private var isShowingAnnotations = false
+    
+    private var allLocations = [Venue]() {
+        didSet{
+            DispatchQueue.main.async {
+                self.searchView.collectionView.reloadData()
+            }
+        }
     }
-  }
-   
-   
-   
-   
-//  init(dataPersistence: DataPersistence<Map>,userPreference: UserPreference) {
-//    self.dataPersistence = dataPersistence
-//    self.userPreference = userPreference
-//    super.init(nibName: nil, bundle: nil)
-//    // self.userPreference.delegate = self
-//  }
-//
-//  required init?(coder: NSCoder) {
-//    fatalError("init couldnt be implemented")
-//  }
-//
-   
-  override func loadView() {
-    view = searchView
-  }
-   
-   
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    view.backgroundColor = .white
-    searchView.collectionView.register(SearchCell.self, forCellWithReuseIdentifier: "SearchCell")
-    searchView.mapView.showsUserLocation = true
-    searchView.collectionView.dataSource = self
-  }
-   
+    
+    private var allPhotos = [Photo]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.searchView.collectionView.reloadData()
+            }
+        }
+    }
+    
+    override func loadView() {
+        view = searchView
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        searchView.collectionView.register(SearchCell.self, forCellWithReuseIdentifier: "SearchCell")
+        searchView.collectionView.dataSource = self
+        searchView.collectionView.delegate = self
+        searchView.topSearchBar.delegate = self
+        searchView.bottomSearchBar.delegate = self
+        searchView.mapView.showsUserLocation = true
+        searchView.searchButton.addTarget(self, action: #selector(searchButtonPressed), for: .touchUpInside)
+        loadMapAnnotation()
+        
+    }
+    
+    
+    private func convertPlaceNameToCoordinate(_ placeName: String) {
+        coreLocation.convertPlaceNameToCoordinate(addressString: placeName)
+        { (result) in
+            switch result {
+            case .failure(let error):
+                print("geocoding error \(error)")
+            case .success(let coordinate):
+                print("coordinate: \(coordinate)")
+                // set map view at given coordinate
+                let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 1600, longitudinalMeters: 1600)
+                self.searchView.mapView.setRegion(region, animated: true)
+            }
+        }
+    }
+    
+    
+    private func loadVenues(city: String, venue: String) {
+        VenueAPIClient.getVenues(city: city, venue: venue, completion: { (result) in
+            switch result {
+            case .failure(let appError):
+                print("error getting venue: \(appError)")
+            case .success(let data):
+                DispatchQueue.main.async {
+                    self.allLocations = data
+                    self.loadMapAnnotation()
+                }
+            }
+        }
+        )}
+    
+    
+    private func makeAnnotations() -> [MKPointAnnotation] {
+        var annotations = [MKPointAnnotation]()
+        for location in allLocations {
+            let latitude = Double(location.location.lat)
+            let longitude = Double(location.location.lng)
+            let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            print("coordinate func makeAnnotations: \(coordinate)")
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = location.name
+            annotations.append(annotation)
+        }
+        dump(annotations)
+        return annotations
+    }
+    
+    
+    
+    func loadMapAnnotation() {
+        let annotations = makeAnnotations()
+        searchView.mapView.addAnnotations(annotations)
+        searchView.mapView.showAnnotations(annotations, animated: true)
+    }
+    
+    
+    
+    
+    
+    
+    @objc private func searchButtonPressed() {
+        guard let searchLocationText = searchView.bottomSearchBar.text else {
+            return
+        }
+        guard let searchVenueText = searchView.topSearchBar.text else {
+            return
+        }
+        convertPlaceNameToCoordinate(searchLocationText)
+        loadVenues(city: searchLocationText, venue: searchVenueText)
+    }
 }
+
+
+
 extension SearchViewController: UICollectionViewDataSource {
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-     return 10
-  }
-   
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchCell", for: indexPath) as? SearchCell else {
-      fatalError()
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
-     
-    cell.backgroundColor = .white
-    return cell
-  }
-   
-   
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchCell", for: indexPath) as? SearchCell else {
+            fatalError()
+        }
+        
+        cell.backgroundColor = .white
+        return cell
+    }
+}
+
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    
+}
+
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.resignFirstResponder()
+        guard let searchText = searchBar.text,
+            !searchText.isEmpty else {
+                return true
+        }
+       // convertPlaceNameToCoordinate(searchText)
+        return true
+    }
+    
+}
+
+
+extension SearchViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        print("didSelect")
+    }
+    
+    
+    
 }
