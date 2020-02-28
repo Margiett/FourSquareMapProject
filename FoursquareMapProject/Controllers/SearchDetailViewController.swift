978e0c39c41736fa090a313d08fb1a70d7d0d748
tanyaburke <tanyaburke@pursuit.org>
2020-02-28 12:36:22 -0500

    Added UI to Detail View

--- FoursquareMapProject/Controllers/SearchDetailViewController.swift
+++ FoursquareMapProject/Controllers/SearchDetailViewController.swift
@@ -7,24 +7,91 @@
 //
 
 import UIKit
+import DataPersistence
 
 class SearchDetailViewController: UIViewController {
-
+    
+    
+    let detailView = SearchDetailView()
+    private var dataPersistence: DataPersistence<Venue>
+    var venue: Venue
+    var photo: Photo
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "star"), style: .plain, target: self, action: #selector(favButtonPressed(_:)))
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    init(_ dataPersistence: DataPersistence<Venue>, venue: Venue, photo: Photo){
+        
+        self.dataPersistence = dataPersistence
+        self.venue = venue
+        self.photo = photo
+        super.init(nibName: nil, bundle: nil)
+        
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        detailView.imageView.layer.cornerRadius = 10
+        detailView.nameLabel.layer.cornerRadius = 10
+        detailView.addressLabel.layer.cornerRadius = 10
+        detailView.addressButton.layer.cornerRadius = 10
+        detailView.phoneLabel.layer.cornerRadius = 10
+        detailView.phoneButton.layer.cornerRadius = 10
+        detailView.detailsText.layer.cornerRadius = 10
+    }
+    override func loadView() {
+        view = detailView
+    }
+    
+    @objc func favButtonPressed(_ sender: UIBarButtonItem){
 
+               do {
+
+                   try dataPersistence.createItem(venue)
+                   DispatchQueue.main.async {
+                       self.showAlert(title: "Saved", message: "Venue has been added to favorites")
+                   }
+
+               } catch {
+                   print("error saving article: \(venue)")
+               }
+       }
+    
+    func imageURL() -> String{
+        let imageUrl = "\(photo.prefix)300*300\(photo.suffix)"
+              return imageUrl
+    }
+    
+    func updateUI() {
+        detailView.imageView.getImage(with: imageURL()  ) { [weak self] (result) in
+            switch result {
+            case .failure:
+                DispatchQueue.main.async {
+                    self?.detailView.imageView.image = UIImage(named: "exclamation.mark.triangle")
+                }
+            case .success(let image):
+                DispatchQueue.main.async {
+                    self?.detailView.imageView.image = image
+                    
+                }
+            }
+        }
+        
+//        detailView.textView.text = chosenBook.description
+        
+        detailView.nameLabel.text = venue.name
+        //detailView.addressLabel.text =
+        
+               //detailView.addressButton
+        //detailView.phoneLabel.text =
+               //detailView.phoneButton
+       // detailView.detailsText.text =
+    
+    }
 }
