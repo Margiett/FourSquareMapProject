970e5210be0bc1c3877684fae7efd0dbf2304a7b
David <lin.david985@gmail.com>
2020-02-29 11:43:27 -0500

    added changes to search controllers

--- FoursquareMapProject/Controllers/SearchTableVC.swift
+++ FoursquareMapProject/Controllers/SearchTableVC.swift
@@ -8,44 +8,62 @@
 
 import UIKit
 import Pulley
+import DataPersistence
+
+
+
 class SearchTableVC: UIViewController {
-   
-  let searchTableView = SearchTableView()
-   
-  private var allLocations = [String]() {
-    didSet{
-      DispatchQueue.main.async {
-        self.searchTableView.searchTableView.reloadData()
-      }
+    
+    let searchTableView = SearchTableView()
+    
+    private var allLocations = [Venue]() {
+        didSet{
+            DispatchQueue.main.async {
+                self.searchTableView.searchTableView.reloadData()
+            }
+        }
     }
-  }
-   
-  override func loadView() {
-    view = searchTableView
-  }
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-   
+    
+    override func loadView() {
+        view = searchTableView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // Hides and shortens the navigation bar
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    // allows the nav bar to show again in the detailVC
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
 }
 extension SearchTableVC: PulleyDrawerViewControllerDelegate {
-  func collapsedDrawerHeight(bottomSafeArea: CGFloat) -> CGFloat {
-    return 120
-  }
+    func collapsedDrawerHeight(bottomSafeArea: CGFloat) -> CGFloat {
+        return 120
+    }
 }
 extension SearchTableVC: UITableViewDataSource {
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
-  }
-   
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    guard let cell = tableView.dequeueReusableCell(withIdentifier: "SearchTableViewCell", for: indexPath) as? SearchTableViewCell else {
-      fatalError()
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    return cell
-  }
-   
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SearchTableViewCell", for: indexPath) as? SearchTableViewCell else {
+            fatalError()
+        }
+        let detailTVC = allLocations[indexPath.row]
+        cell.configureCell(venue: detailTVC)
+        cell.backgroundColor = .white
+        return cell
+    }
+    
 }
 extension SearchTableVC: UITableViewDelegate {
-   
+    
 }
--- FoursquareMapProject/Views/Cell/SearchCell.swift
+++ FoursquareMapProject/Views/Cell/SearchCell.swift
@@ -10,4 +10,89 @@ import UIKit
 
 class SearchCell: UICollectionViewCell {
     
+    public lazy var imageView : UIImageView = {
+      let imageView = UIImageView()
+      imageView.backgroundColor = .systemGray3
+      imageView.contentMode = .scaleAspectFill
+      return imageView
+    }()
+    
+    public lazy var venueNameLabel : UILabel = {
+      let label = UILabel()
+      label.text = "Venue Name"
+      label.font = label.font.withSize(20)
+      label.textAlignment = .left
+      return label
+    }()
+    
+    public lazy var addressLabel : UILabel = {
+      let label = UILabel()
+      label.text = "Venue Address"
+      label.textAlignment = .left
+      return label
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder:coder)
+        commonInit()
+    }
+    
+    
+    private func commonInit() {
+        configureImage()
+        configureVenueNameLabel()
+        configureAddress()
+    }
+    
+    
+    
+    func configureCell(venue: Venue) {
+        venueNameLabel.text = venue.name
+        addressLabel.text = venue.location.address
+        
+        
+        
+    }
+    
+    
+    private func configureImage() {
+        addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 100)
+        ])
+    }
+    
+    private func configureVenueNameLabel() {
+        addSubview(venueNameLabel)
+        venueNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            venueNameLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 15),
+            venueNameLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 10),
+            venueNameLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -10)
+        ])
+    }
+    
+    private func configureAddress() {
+        addSubview(addressLabel)
+        addressLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            addressLabel.topAnchor.constraint(equalTo: venueNameLabel.bottomAnchor, constant: 10),
+            addressLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 10),
+            addressLabel.trailingAnchor.constraint(equalTo: venueNameLabel.trailingAnchor)
+        ])
+    }
+    
+    
+    
+    
 }
